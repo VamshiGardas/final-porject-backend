04c5da657ff08a2636b5011541f3ce65c32d4a65
vamshi <sg18975n@pace.edu>
2023-12-11 11:11:48 -0500

    Initial commit

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.json());
 
 // MongoDB URL - replace with your actual MongoDB URL
 const mongoDBUrl =
-  "mongodb+srv://VAMSHI:abcd123@cluster1.jrwompw.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://VAMSHI:abcd123@cluster1.jrwompw.mongodb.net/yourDatabaseName?retryWrites=true&w=majority";
 
-// Connect to MongoDB
+// Connect to MongoDB with updated connection options
 mongoose
-  .connect(mongoDBUrl)
+  .connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
@@ -32,17 +32,30 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model("Note", noteSchema);
 
 // Routes
+// Get all notes
 app.get("/notes", async (req, res) => {
-  const notes = await Note.find();
-  res.send(notes);
+  try {
+    const notes = await Note.find();
+    res.send(notes);
+  } catch (err) {
+    console.error("Error fetching notes", err);
+    res.status(500).send("Error fetching notes");
+  }
 });
 
+// Add a new note
 app.post("/notes", async (req, res) => {
-  const newNote = new Note(req.body);
-  await newNote.save();
-  res.send(newNote);
+  try {
+    const newNote = new Note(req.body);
+    await newNote.save();
+    res.send(newNote);
+  } catch (err) {
+    console.error("Error saving the note", err);
+    res.status(500).send("Error saving the note");
+  }
 });
 
+// Delete a note
 app.delete("/notes/:id", async (req, res) => {
   try {
     const result = await Note.findByIdAndDelete(req.params.id);
@@ -50,7 +63,7 @@ app.delete("/notes/:id", async (req, res) => {
       return res.status(404).send("The note with the given ID was not found.");
     res.send(result);
   } catch (err) {
-    console.error(err);
+    console.error("Error deleting the note", err);
     res.status(500).send("Error deleting the note");
   }
 });
@@ -58,3 +71,5 @@ app.delete("/notes/:id", async (req, res) => {
 // Start server on port 5000
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = app; // Exporting the app for potential testing or further extension
