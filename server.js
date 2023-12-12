de1baad21967a15ae9f237f898ae8f8405649036
vamshi <sg18975n@pace.edu>
2023-12-12 17:22:45 -0500

    frontend url2

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const app = express();
 // Update CORS settings to allow requests from your Vercel frontend URL
 app.use(
   cors({
-    origin:
-      "https://final-project-frontend-ezsjr7mlr-vamshi-gardas-projects.vercel.app",
+    origin: "https://final-project-frontend-liart.vercel.app/",
   })
 ); // Replace with your Vercel frontend URL
 
