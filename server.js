f3e9e03cd9a5c0e1af63602f57abdebf15dba125
vamshi <sg18975n@pace.edu>
2023-12-13 06:56:55 -0500

    local

--- server.js
+++ server.js
@@ -2,67 +2,65 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+// Initialize express app
 const app = express();
 
-// Updated CORS origin without trailing slash
-app.use(
-  cors({
-    origin: "https://final-project-frontend-liart.vercel.app",
-  })
-);
+// Enable CORS for frontend interactions
+app.use(cors());
 
+// Body parser middleware to handle JSON data
 app.use(express.json());
 
-// Hardcoded MongoDB URL
+// MongoDB URL - replace with your actual MongoDB URL
 const mongoDBUrl =
-  "mongodb+srv://VAMSHI:abcd123@cluster1.jrwompw.mongodb.net/yourDatabaseName?retryWrites=true&w=majority";
+  "mongodb+srv://VAMSHI:abcd123@cluster0.9druc5w.mongodb.net/?retryWrites=true&w=majority";
+
+// Connect to MongoDB with updated connection options
 mongoose
   .connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
-// Define Note Schema and Model with basic validation
+// Define Note Schema and Model
 const noteSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
+  title: String,
+  content: String,
   font: String,
   color: String,
 });
 
 const Note = mongoose.model("Note", noteSchema);
 
-// Routes with added logging
+// Routes
+// Get all notes
 app.get("/notes", async (req, res) => {
   try {
     const notes = await Note.find();
-    console.log("Fetched notes:", notes); // Added for debugging
     res.send(notes);
   } catch (err) {
     console.error("Error fetching notes", err);
-    res.status(500).send("Server error");
+    res.status(500).send("Error fetching notes");
   }
 });
 
+// Add a new note
 app.post("/notes", async (req, res) => {
   try {
     const newNote = new Note(req.body);
     await newNote.save();
-    console.log("Saved new note:", newNote); // Added for debugging
-    res.status(201).send(newNote);
+    res.send(newNote);
   } catch (err) {
     console.error("Error saving the note", err);
-    res.status(400).send("Error saving the note");
+    res.status(500).send("Error saving the note");
   }
 });
 
+// Delete a note
 app.delete("/notes/:id", async (req, res) => {
   try {
     const result = await Note.findByIdAndDelete(req.params.id);
-    if (!result) {
-      console.log("Note not found with ID:", req.params.id); // Added for debugging
+    if (!result)
       return res.status(404).send("The note with the given ID was not found.");
-    }
-    console.log("Deleted note:", result); // Added for debugging
     res.send(result);
   } catch (err) {
     console.error("Error deleting the note", err);
@@ -70,7 +68,8 @@ app.delete("/notes/:id", async (req, res) => {
   }
 });
 
-const port = 5000; // Hardcoded port number
+// Start server on port 5000
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
-module.exports = app;
+module.exports = app; // Exporting the app for potential testing or further extension
